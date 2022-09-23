9d75929ccfbcc4d32ce55c7c4e496a2a27a1554f
Gsrilakshmi-123 <k.p.s.lakashmi.98@gmail.com>
2022-09-24 00:34:53 +0530

    First commit

---
+++ exception/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package net.javaguides.govtquestion.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID=1L;
+	
+	
+	
+
+
+	public ResourceNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+	}
+}
+
+
+
+
+	
---
+++ repository/Question1Repository.java
@@ -0,0 +1,24 @@
+package net.javaguides.govtquestion.repository;
+
+
+
+import java.util.Optional;
+import java.util.function.IntPredicate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguides.govtquestion.model.Questions1;
+@Repository
+public interface Question1Repository extends JpaRepository<Questions1,Long>{
+
+
+
+	
+	
+
+	
+
+	
+
+}
